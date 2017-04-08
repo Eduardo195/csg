d8dc946a15b956d2b8bccff297dc2160a8798ce4
mbssantos <lamppostman@gmail.com>
2017-04-08 09:41:22 +0100

    misc: ui improvements

--- client/components/footer/footer.jsx
+++ client/components/footer/footer.jsx
@@ -3,21 +3,21 @@ import Link from 'components/link/link';
 
 const Footer = () => (
   <footer>
-    <div className="d-flex justify-content-center flex-wrap text-center">
-      <div className="flex-grow align-self-stretch">
+    <div className="d-flex justify-content-center flex-wrap">
+      <div className="align-self-stretch">
         <article>
           <h4>For Opportunity Seekers</h4>
           <div><Link href="/opportunities">Find Opportunities</Link></div>
           <div><Link href="#">Upload CV</Link></div>
         </article>
       </div>
-      <div className="flex-grow align-self-stretch">
+      <div className="align-self-stretch">
         <article>
           <h4>For Employers</h4>
           <div><Link href="/opportunity/create">Create opportunity</Link></div>
         </article>
       </div>
-      <div className="flex-grow align-self-stretch">
+      <div className="align-self-stretch">
         <article>
           <h4>About us</h4>
           <div><Link href="#">Privacy</Link></div>
