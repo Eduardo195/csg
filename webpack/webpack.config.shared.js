a87799d3c69d9eaa47ba1fef51632e8156308a99
mbssantos <lamppostman@gmail.com>
2017-04-08 16:40:53 +0100

    misc: changed loading gif

--- webpack/webpack.config.shared.js
+++ webpack/webpack.config.shared.js
@@ -17,11 +17,12 @@ const config = {
     rules: [
       { test: /\.js$/, use: 'babel-loader', exclude: EXCLUDE },
       { test: /\.jsx$/, use: 'babel-loader', exclude: EXCLUDE },
-      { test: /\.svg$/, use: 'file-loader', exclude: EXCLUDE }
+      { test: /\.svg$/, use: 'file-loader', exclude: EXCLUDE },
+      { test: /\.gif$/, use: 'file-loader', exclude: EXCLUDE }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.jpg', '.svg', '.less'],
+    extensions: ['.js', '.jsx', '.jpg', '.svg', '.gif', '.less'],
     modules: [
       path.resolve(__dirname, '../client'),
       path.resolve(__dirname, '../node_modules/'),
